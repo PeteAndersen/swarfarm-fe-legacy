f6ca8e7d3be86457ce227fb76be27677b2fe744f
porksmash <peter@porksmash.com>
2017-07-03 20:35:10 -0700

    Footer

---
+++ src/ui/components/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Menu, Icon } from "semantic-ui-react";
+
+const Footer = () => (
+  <Menu widths={3}>
+    <Menu.Item>
+      <a
+        href="https://github.com/porksmash/swarfarm"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon name="github" /> Github
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      Like the site? <a href="https://swarfarm.com/donate/">&nbsp;Donate!</a>
+    </Menu.Item>
+    <Menu.Item>
+      <Icon name="copyright" />
+      {" "}
+      Porksmash 2017 ⋅ Images
+      {" "}
+      <Icon name="copyright" />
+      {" "}
+      Com2Us
+    </Menu.Item>
+  </Menu>
+);
+
+export default Footer;
--- src/ui/layout/app.js
+++ src/ui/layout/app.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import styled from "styled-components";
 
 import MainMenu from "ui/components/MainMenu";
+import Footer from "ui/components/Footer";
 import { Home, News, Bestiary } from "ui/pages";
 
-const App = () => (
-  <div>
-    <MainMenu />
+const Body = styled.div`
+display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
 
-    <Container fluid>
+const Content = styled.div`
+  flex: 1;
+`;
+
+const App = () => (
+  <Body>
+    <MainMenu />
+    <Content>
       <Route path="/" component={Home} exact />
       <Route path="/news" component={News} />
       <Route path="/bestiary" component={Bestiary} />
-    </Container>
-
-    <footer>
-      Da footer.
-    </footer>
-  </div>
+    </Content>
+    <Footer />
+  </Body>
 );
 
 export default App;
