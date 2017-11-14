ef354a5a1790b636ce61cb759e78c8de24ea2d19
Peter Andersen <peter@porksmash.com>
2017-11-14 14:25:59 -0800

    Commenting out authSagas fixes resolution of authReducer

--- src/state/ducks/auth/index.js
+++ src/state/ducks/auth/index.js
@@ -1,7 +1,7 @@
 import reducer from './reducers';
 
 export { default as authActions } from './actions';
-export { default as authRootSaga } from './sagas';
+//export { default as authRootSaga } from './sagas';
 export { default as authApi } from './api';
 
 export default reducer;
--- src/state/ducks/sagas.js
+++ src/state/ducks/sagas.js
@@ -1,9 +1,9 @@
 import { all, spawn } from 'redux-saga/effects';
 
-import { authRootSaga } from './auth';
+//import { authRootSaga } from './auth';
 import { newsRootSaga } from './news';
 import { bestiaryRootSaga } from './bestiary';
 
 export default function*() {
-  yield all([spawn(authRootSaga), spawn(newsRootSaga), spawn(bestiaryRootSaga)]);
+  yield all([/*spawn(authRootSaga),*/ spawn(newsRootSaga), spawn(bestiaryRootSaga)]);
 }
