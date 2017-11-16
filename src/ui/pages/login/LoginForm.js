daf679697fefed479d624f7bbdae074581aaf704
Peter Andersen <peter@porksmash.com>
2017-11-16 13:22:10 -0800

    Faking semantic-ui form fields via css, easiest way to get Formik + Semantic UI working together

--- src/ui/pages/login/LoginForm.js
+++ src/ui/pages/login/LoginForm.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import classnames from 'classnames';
 import { Formik, Form, Field } from 'formik';
 import Yup from 'yup';
-//import { Form, Input, Message } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
+
+import TextInput from 'ui/components/form/textinput';
 
 const LoginForm = ({ handleSubmit }) => {
   return (
@@ -24,17 +27,34 @@ const LoginForm = ({ handleSubmit }) => {
         handleBlur,
         handleSubmit,
         isSubmitting
-      }) => (
-        <Form onSubmit={handleSubmit}>
-          <Field type="input" name="username" label="username" component={Form.Input} />
-          {touched.username && errors.username && <div>{errors.username}</div>}
+      }) => {
+        const formClasses = classnames('ui', 'form', { error: isValid, loading: isSubmitting });
+        return (
+          <Form onSubmit={handleSubmit} className={formClasses}>
+            <TextInput
+              type="text"
+              name="username"
+              label="Username"
+              error={touched.username && errors.username}
+              value={values.username}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
 
-          <Field type="password" name="password" label="password" component={Form.Input} />
-          {touched.password && errors.password && <div>{errors.password}</div>}
+            <TextInput
+              type="password"
+              name="password"
+              label="Password"
+              error={touched.password && errors.password}
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
 
-          <button disabled={isSubmitting || !isValid}>Submit</button>
-        </Form>
-      )}
+            <Button disabled={isSubmitting || !isValid}>Submit</Button>
+          </Form>
+        );
+      }}
     />
   );
 };
