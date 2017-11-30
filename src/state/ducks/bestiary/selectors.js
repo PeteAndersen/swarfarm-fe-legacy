4ac999cf1798abbc6ab2bfacec9de9ddd8441747
Peter Andersen <peter@porksmash.com>
2017-11-30 08:36:00 -0800

    Extended functionality of selector functions to return all or specific entities

--- src/state/ducks/bestiary/selectors.js
+++ src/state/ducks/bestiary/selectors.js
@@ -1,5 +1,24 @@
 import { createSelector } from 'reselect';
 
+const _getEntityHelper = (entities, id) => {
+  //console.log(entities, id);
+  if (id) {
+    if (Array.isArray(id)) {
+      // Array of IDs - return object of entities
+      return id.reduce((effects, effect_id) => {
+        effects[effect_id] = entities[effect_id];
+        return effects;
+      }, {});
+    } else {
+      // Return single entity
+      return entities[id];
+    }
+  } else {
+    // Return all entities
+    return entities;
+  }
+};
+
 // UI
 const isLoading = state => state.bestiary.isLoading;
 const isPopulating = state => state.bestiary.isPopulating;
@@ -7,26 +26,28 @@ const wasPopulated = state => state.bestiary.wasPopulated;
 const lastPopulated = state => state.bestiary.lastPopulated;
 
 // Bestiary Entities
-const getMonsterEntity = (state, id) => state.bestiary.entities.monsters[id];
-const getMonsters = state => state.bestiary.entities.monsters;
-const getSkills = state => state.bestiary.entities.skills;
-const getLeaderSkills = state => state.bestiary.entities.leaderSkills;
-const getSources = state => state.bestiary.entities.sources;
+const getMonsters = (state, id) => _getEntityHelper(state.bestiary.entities.monsters, id);
+const getSkills = (state, id) => _getEntityHelper(state.bestiary.entities.skills, id);
+const getEffects = (state, id) => _getEntityHelper(state.bestiary.entities.effects, id);
+const getLeaderSkills = (state, id) => _getEntityHelper(state.bestiary.entities.leaderSkills, id);
+const getSources = (state, id) => _getEntityHelper(state.bestiary.entities.sources, id);
 
 const getMonsterList = createSelector(
   getMonsters,
   getSkills,
   getLeaderSkills,
   getSources,
-  (monsters, skills, leaderSkills, sources) =>
-    Object.values(monsters)
+  (monsters, skills, leaderSkills, sources) => {
+    //console.log(monsters, skills, leaderSkills, sources);
+    return Object.values(monsters)
       .sort((a, b) => (a.name > b.name ? 1 : -1))
       .map(monster => ({
         ...monster,
         skills: monster.skills ? monster.skills.map(skillId => skills[skillId]) : null,
         leaderSkill: monster.leaderSkill ? leaderSkills[monster.leaderSkill] : null,
         source: monster.source ? monster.source.map(sourceId => sources[sourceId]) : null
-      }))
+      }));
+  }
 );
 
 const getObtainableMonsterList = createSelector(getMonsterList, monsters =>
@@ -37,31 +58,15 @@ const getObtainableMonsterList = createSelector(getMonsterList, monsters =>
 // TODO: Add bestiary filters here
 const getVisibleMonsterList = createSelector(getObtainableMonsterList, monsters => monsters);
 
-const getMonster = createSelector(
-  getMonsterEntity,
-  getMonsters,
-  getSkills,
-  getLeaderSkills,
-  getSources,
-  (monster, monsters, skills, leaderSkills, sources) => ({
-    ...monster,
-    skills: monster.skills ? monster.skills.map(skillId => skills[skillId]) : null,
-    leaderSkill: monster.leaderSkill ? leaderSkills[monster.leaderSkill] : null,
-    source: monster.source ? monster.source.map(sourceId => sources[sourceId]) : null,
-    awakens_to: monster.awakens_to ? monsters[monster.awakens_to] : null,
-    awakens_from: monster.awakens_from ? monsters[monster.awakens_from] : null
-  })
-);
-
 export default {
   isLoading,
   isPopulating,
   wasPopulated,
   lastPopulated,
-  getMonsterEntity,
-  getMonster,
   getMonsters,
   getMonsterList,
   getObtainableMonsterList,
+  getSkills,
+  getEffects,
   getVisibleMonsterList
 };
